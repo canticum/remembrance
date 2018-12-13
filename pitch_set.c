dd1b82d4c9b799362c9aa731ea4e42b3b045e55d
Jonathan Chang, Chun-yien <ccy@musicapoetica.org>
2018-12-13 18:28:03 +0800

    update pitchset code; add result.txt

---
+++ pitch_set.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+	const int bars=100, tone=5;
+	int result[bars][tone];
+	int r;
+	srand((unsigned)time(NULL));
+	for(int i = 0; i < bars; i++)
+	{
+		printf (" %d : ", i);
+		for(int j = 0; j < tone; j++)
+		{
+			r = rand();
+			result[i][j] = r - int (r / 12)*12;
+			printf (" %d, ", result[i][j]);
+		}
+		printf ("\n");
+	}
+    return 0;
+}
